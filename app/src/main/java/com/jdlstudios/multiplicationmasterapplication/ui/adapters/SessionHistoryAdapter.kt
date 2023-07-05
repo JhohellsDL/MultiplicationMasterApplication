a9962cd136db5b5e9f47ce68e1c68d5575924383
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2023-07-05 00:36:41 -0500

    adapter ok, view history ok

--- app/src/main/java/com/jdlstudios/multiplicationmasterapplication/ui/adapters/SessionHistoryAdapter.kt
+++ app/src/main/java/com/jdlstudios/multiplicationmasterapplication/ui/adapters/SessionHistoryAdapter.kt
@@ -7,22 +7,35 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.jdlstudios.multiplicationmasterapplication.R
 import com.jdlstudios.multiplicationmasterapplication.data.local.models.SessionEntity
+import com.jdlstudios.multiplicationmasterapplication.domain.models.Difficulty
 
-class SessionHistoryAdapter(): RecyclerView.Adapter<SessionHistoryAdapter.ViewHolder>(){
+class SessionHistoryAdapter : RecyclerView.Adapter<SessionHistoryAdapter.ViewHolder>() {
 
     var data = listOf<SessionEntity>()
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val answerCorrects: TextView = itemView.findViewById(R.id.correct_session_textview)
+        private val answerCorrects: TextView = itemView.findViewById(R.id.correct_session_textview)
+        private val answerIncorrects: TextView =
+            itemView.findViewById(R.id.incorrect_session_textview)
+        private val difficultyText: TextView = itemView.findViewById(R.id.text_session_difficulty)
+        private val dateText: TextView = itemView.findViewById(R.id.date_session_textview)
+        private val scoreText: TextView = itemView.findViewById(R.id.score_session_textview)
+        private val exercisesText: TextView =
+            itemView.findViewById(R.id.number_exercises_session_textview)
 
         fun bind(
             item: SessionEntity
-        ){
+        ) {
             answerCorrects.text = item.correctAnswers.toString()
+            answerIncorrects.text = item.incorrectAnswers.toString()
+            difficultyText.text = Difficulty.getDifficultyFromInt(item.difficulty).toString()
+            dateText.text = item.timestamp.toString()
+            scoreText.text = String.format("%d pts", item.score)
+            exercisesText.text = String.format("%d exercises", item.numberOfExercises)
         }
 
-        companion object{
+        companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val view = layoutInflater.inflate(R.layout.item_session, parent, false)
@@ -30,6 +43,7 @@ class SessionHistoryAdapter(): RecyclerView.Adapter<SessionHistoryAdapter.ViewHo
             }
         }
     }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
